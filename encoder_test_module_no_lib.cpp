ddc62b206754acfd07131dfc40a1f262d9c06348
Ing. Di Filippo <davidedifilippo@gmail.com>
2022-02-09 20:56:00 +0100

    Encoder software test module without support library

---
+++ encoder_test_module_no_lib.cpp
@@ -0,0 +1,51 @@
+#include <Arduino.h>
+
+//PIN's definition
+
+#define pin_canaleA  2
+#define pin_canaleB  3
+
+volatile int conta_impulsi = 0;
+
+volatile boolean A = 0;
+volatile boolean B = 0;
+
+void doEncoderA()
+{
+     B ? conta_impulsi--:  conta_impulsi++;
+    
+}
+
+void doEncoderB()
+{
+     B = !B; 
+}
+
+void setup() 
+{
+  Serial.begin (9600);
+   
+   //turn on pullup resistor
+  pinMode(pin_canaleA, INPUT_PULLUP);
+  pinMode(pin_canaleB, INPUT_PULLUP);
+  
+
+
+  A = (boolean)digitalRead(pin_canaleA); //valore iniziale canale A
+  B = (boolean)digitalRead(pin_canaleB); //valore iniziale canale B
+
+
+  attachInterrupt(digitalPinToInterrupt(pin_canaleA), doEncoderA, RISING);
+
+  attachInterrupt(digitalPinToInterrupt(pin_canaleB), doEncoderB, CHANGE); 
+
+}
+
+
+void loop()
+{  
+  Serial.print ("Count = ");  
+    Serial.println (conta_impulsi);
+    delay(10);
+}
+

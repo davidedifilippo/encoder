8b2bf531e7e869e1350912c6253777bcee488492
Ing. Di Filippo <davidedifilippo@gmail.com>
2026-01-08 09:35:30 +0100

    Refactor rotary encoder pin definitions and ISR

--- EC11_rotary_encoder_test.ino
+++ EC11_rotary_encoder_test.ino
@@ -1,8 +1,10 @@
-#define ENCODER_A 2
-#define ENCODER_B 3
+#define A_CHANNEL 2
+#define B_CHANNEL 3
 
 volatile long count = 0;
 unsigned long lastInterruptTime = 0;
+
+long lastCount = 0;
 
 void ISR_A() {
   // Debouncing software semplice: ignora impulsi troppo ravvicinati (< 5ms)
@@ -10,7 +12,8 @@ void ISR_A() {
   if (interruptTime - lastInterruptTime > 5) {
     // Se A sale e B è BASSO, va in una direzione
     // Se A sale e B è ALTO, va nell'altra
-    if (digitalRead(ENCODER_B) == LOW) {
+    
+    if (digitalRead(B_CHANNEL) == LOW) {
       count++;
     } else {
       count--;
@@ -20,17 +23,21 @@ void ISR_A() {
 }
 
 void setup() {
-  Serial.begin(115200); // 9600 è troppo lento, usa 115200
+  Serial.begin(115200); 
   
-  // Usa le resistenze di pull-up interne
-  pinMode(ENCODER_A, INPUT);
-  pinMode(ENCODER_B, INPUT);
-  
-  attachInterrupt(digitalPinToInterrupt(ENCODER_A), ISR_A, RISING);
+  // Imposto i piedini 
+  pinMode(A_CHANNEL, INPUT);
+  pinMode(B_CHANNEL, INPUT);
+
+   //Attivo l'interrupt sul piedino 2
+  //Un fronte di salita sul canale A dell'encoder innesca la routine ISR_A
+  attachInterrupt(digitalPinToInterrupt(A_CHANNEL), ISR_A, RISING); 
 }
 
 void loop() {
-  static long lastCount = 0;
+  
+  //Stampo solo se ho un incremento/decremento
+
   if (count != lastCount) {
     Serial.println(count);
     lastCount = count;
